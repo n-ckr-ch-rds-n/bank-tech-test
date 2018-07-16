6678965521fe83af7286855f282d3f59a05239ed
Nick <nicholasjohnrichardson@gmail.com>
2018-07-16 11:33:42 +0100

    Create Account object and deposit function

--- jasmine/spec/AccountSpec.js
+++ jasmine/spec/AccountSpec.js
@@ -2,11 +2,12 @@ describe("Account", function() {
 
   describe("#deposit", function() {
 
+    var account = new Account;
+
     it("increases the account balance", function() {
-      var account = new Account;
       account.deposit(100);
       expect(account.balance).toBe(100);
-    })
+    });
 
   })
 })
