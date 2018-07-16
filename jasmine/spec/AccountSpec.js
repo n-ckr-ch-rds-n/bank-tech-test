6a2ddd813d19efffa5f940adaa43175698d7cbb6
Nick <nicholasjohnrichardson@gmail.com>
2018-07-16 11:53:03 +0100

    Create #logTransaction

--- jasmine/spec/AccountSpec.js
+++ jasmine/spec/AccountSpec.js
@@ -1,12 +1,23 @@
 describe("Account", function() {
 
-  describe("#deposit", function() {
+  var account = new Account;
 
-    var account = new Account;
+  describe("#deposit", function() {
 
     it("increases the account balance", function() {
       account.deposit(100);
       expect(account.balance).toBe(100);
+    });
+
+  })
+
+  describe("#logTransaction", function() {
+
+    it("logs transactions in the transaction ledger", function() {
+      account.logTransaction("14/01/2012", 100, "Deposit")
+      expect(account.transactions[0][0]).toBe("14/01/2012")
+      expect(account.transactions[0][1]).toBe(100)
+      expect(account.transactions[0][2]).toBe("Deposit")
     });
 
   })
--- jasmine/src/Account.js
+++ jasmine/src/Account.js
@@ -1,7 +1,12 @@
 function Account() {
   this.balance = 0;
+  this.transactions = [];
 };
 
 Account.prototype.deposit = function (amount) {
   this.balance += amount;
+};
+
+Account.prototype.logTransaction = function (date, amount, type) {
+  this.transactions.push([date, amount, type]);
 };
