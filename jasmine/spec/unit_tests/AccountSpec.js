c213520895b99f9d4f9037f39210f654cfc408fb
Nick <nicholasjohnrichardson@gmail.com>
2018-07-17 12:20:50 +0100

    fixed unit tests for Account class

--- jasmine/spec/unit_tests/AccountSpec.js
+++ jasmine/spec/unit_tests/AccountSpec.js
@@ -1,52 +1,40 @@
 describe("Account", function() {
 
-  var account = new Account;
+  var account;
+
+  beforeEach(function() {
+    account = new Account;
+  })
 
   describe("#deposit", function() {
 
     it("increases the account balance", function() {
-      account.balance = 0;
-      account.deposit(100);
-      expect(account.balance).toBe(100);
+      account.increaseBalance(100);
+      expect(account._balance).toBe(100);
     });
 
-    it("writes to #logTransaction", function() {
-      account.balance = 0;
-      account.transactions = [];
-      account.deposit(100);
-      expect(account.transactions.length).toBe(1);
-    })
+    it("writes to _ledger.logTransaction", function() {
+      spyOn(account._ledger, 'logTransaction')
+      account.increaseBalance(100);
+      expect(account._ledger.logTransaction).toHaveBeenCalled();
+    });
 
   })
 
   describe("#withdraw", function() {
 
     it("decreases the account balance", function() {
-      account.balance = 50;
-      account.withdraw(50);
-      expect(account.balance).toBe(0);
+      account.increaseBalance(100);
+      account.decreaseBalance(50);
+      expect(account._balance).toBe(50);
     });
 
-    it("writes to #logTransaction", function() {
-      account.balance = 50;
-      account.transactions = [];
-      account.withdraw(10);
-      expect(account.transactions.length).toBe(1);
-      expect(account.transactions[0][2]).toBe("Withdrawal");
-    })
-
-  })
-
-  describe("#logTransaction", function() {
-
-    it("logs transactions in the transaction ledger", function() {
-      account.transactions = [];
-      account.logTransaction("14/01/2012", 100, "Deposit")
-      expect(account.transactions[0][0]).toBe("14/01/2012")
-      expect(account.transactions[0][1]).toBe(100)
-      expect(account.transactions[0][2]).toBe("Deposit")
+    it("writes to _ledger.logTransaction", function() {
+      spyOn(account._ledger, 'logTransaction')
+      account.increaseBalance(100);
+      account.decreaseBalance(50);
+      expect(account._ledger.logTransaction).toHaveBeenCalled();
     });
 
-  })
-
-})
+  });
+});
